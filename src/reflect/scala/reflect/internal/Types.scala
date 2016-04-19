19cb157d3e20cc857ae992deafdfc5eaccf198a9
Miles Sabin <miles@milessabin.com>
2016-04-19 09:56:57 +0100

    Removed dead code.

--- src/reflect/scala/reflect/internal/Types.scala
+++ src/reflect/scala/reflect/internal/Types.scala
@@ -3136,7 +3136,6 @@ trait Types
             val rhs = if (isLowerBound) typeArgs else tp.typeArgs
             // This is a higher-kinded type var with same arity as tp.
             // If so (see SI-7517), side effect: adds the type constructor itself as a bound.
-            val saved = undoLog.log
             val withinBounds = isWithinBounds(prefix, origin.typeSymbol.owner, origin.typeSymbol :: Nil, tp.typeConstructor :: Nil)
             withinBounds && isSubArgs(lhs, rhs, params, AnyDepth) && {
               addBound(tp.typeConstructor); true
