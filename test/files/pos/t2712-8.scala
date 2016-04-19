13e91c4b5330bda123212dc2701fad2dd7b29edf
Miles Sabin <miles@milessabin.com>
2016-04-19 09:53:04 +0100

    Removed stale comments.

--- test/files/pos/t2712-8.scala
+++ test/files/pos/t2712-8.scala
@@ -7,7 +7,7 @@ trait Map1[A, B] extends Other[(A, B)] with Iterable[(A, B)]
 class Test {
   def foo[M[+A] <: Iterable[A]](m: M[Any]): M[Any] = ???
   def test(m: Map[Int, String]) = {
-    foo(m) // fails under -Yhigher-order-unification
+    foo(m)
   }
 
   def test1(m: Map1[Int, String]) = {
